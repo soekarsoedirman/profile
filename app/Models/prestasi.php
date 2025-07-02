78b1cb634df9a410e81d3306dd1feac10e2bf03b
soekarsoedirman <sabiqalhimam3@gmail.com>
2025-07-02 15:50:56 +0700

    create table

---
+++ app/Models/prestasi.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class prestasi extends Model
+{
+    use HasFactory;
+    protected $table = 'prestasi_tablee';
+    protected $fillable = [
+        'judul',
+        'tanggal',
+    ];
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+            @include('layouts.partial.navigation')
 
             <!-- Page Heading -->
             @isset($header)
@@ -32,5 +32,6 @@
                 {{ $slot }}
             </main>
         </div>
+        @include('layouts.partial.footer')
     </body>
 </html>
